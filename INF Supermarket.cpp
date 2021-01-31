3b213fdce6c197dfaaaa867bdcad8b864bf7a695
psitc190049 <55810964+CojoMajesty@users.noreply.github.com>
2021-01-31 22:56:15 +0000

    Add files via upload

---
+++ INF Supermarket.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Thanks for visting Us" <<endl;
+
+int printerPrice = 30;
+int phonePrice = 50;
+int tableprice = 100;
+const int vat = 0.012;
+
+    cout <<"Items you can purchase from us" <<endl;
+    cout <<"Printer" <<endl;
+    cout <<"Phone" <<endl;
+    cout <<"Table" <<endl;
+
+
+    int name;
+    cout<<"Please enter your name"<<endl;
+    cin>> name;
+
+    int unique_ID;
+    cout<<"Please enter your Unique ID"<<endl;
+    cin>> unique_ID;
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
